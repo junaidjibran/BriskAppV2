116a64be721e52de3bf4a320d71ecfcd7c1d1d43
junaidjibran <junaidrana52@gmail.com>
2024-05-26 20:21:59 +0500

    revert db file

--- app/db.server.js
+++ app/db.server.js
@@ -1,33 +1,33 @@
-// import { PrismaClient } from "@prisma/client";
-
-// const prisma = global.prisma || new PrismaClient();
-
-// if (process.env.NODE_ENV !== "production") {
-//   if (!global.prisma) {
-//     global.prisma = new PrismaClient();
-//   }
-// }
-
-// export default prisma;
-
 import { PrismaClient } from "@prisma/client";
 
-let prisma;
+const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-  prisma.$connect().catch((error) => {
-    console.error("Failed to connect to the database in production:", error);
-    process.exit(1); // Exit the process with a failure code
-  });
-} else {
+if (process.env.NODE_ENV !== "production") {
   if (!global.prisma) {
     global.prisma = new PrismaClient();
-    global.prisma.$connect().catch((error) => {
-      console.error("Failed to connect to the database in development:", error);
-    });
   }
-  prisma = global.prisma;
 }
 
 export default prisma;
+
+// import { PrismaClient } from "@prisma/client";
+
+// let prisma;
+
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+//   prisma.$connect().catch((error) => {
+//     console.error("Failed to connect to the database in production:", error);
+//     process.exit(1); // Exit the process with a failure code
+//   });
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//     global.prisma.$connect().catch((error) => {
+//       console.error("Failed to connect to the database in development:", error);
+//     });
+//   }
+//   prisma = global.prisma;
+// }
+
+// export default prisma;
